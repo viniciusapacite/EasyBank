6beff5d80eecd4f7f1cee9281779e4b8c426b38a
vinicius apacite <viniapacite14@gmail.com>
2022-12-22 17:04:29 -0300

    aprimorando o javaScript

--- menuToggle.js
+++ menuToggle.js
@@ -1,15 +1,16 @@
+function chamada(){  
+    var link = document.querySelector('.links').classList.add('#adc');
+    var menu = document.querySelector('.menu-toggle')
+    .addEventListener('click', ()=>{
+        if(link = document.querySelector('.links').style.display == "flex"){
+             link = document.querySelector('.links').style.display = 'none';
+             let trocaImg = document.querySelector('.menu-toggle > img').src = "img/icon-hamburger.svg";
+            } else {
+                let trocaImg = document.querySelector('.menu-toggle > img').src = "img/icon-close.svg";
+                link = document.querySelector('.links').style.display = 'flex';
+            }
+        })
+    }
 
-
- function chamada(){  
-    var link = document.querySelector('.links');
-    const menu = document.querySelector('.menu-toggle')
-    .addEventListener('click', (t)=>{
-        if(link = document.querySelector('.links').style.display == "none"){
-            var link = document.querySelector('.links').style.display = 'flex';
-        }else {
-            var link = document.querySelector('.links').style.display = 'none';
-        }
-    })
-}
 
 chamada();
